804304c5dcbc80200f11764a02d2cf9ce4b9c704
Tim Wheeler <tim@timwheeler.com>
2018-07-03 00:11:56 -0400

    initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.')
+    }
+    console.log('Connected to MongoDB server.');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Become a serial entrepreneur',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection('Users').insertOne({
+        name: 'Tim Wheeler',
+        age: 27,
+        location: 'Boston, MA'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to create user', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    });
+
+    client.close();
+});
